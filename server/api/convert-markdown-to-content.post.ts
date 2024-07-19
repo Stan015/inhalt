06cb97ee8bb1317c1d73a2a33f9da6205fe54e22
Stanley Azi <azistanley17@gmail.com>
2024-07-19 12:22:37 +0100

    add: convert markdown to html for client to render

--- server/api/convert-markdown-to-content.post.ts
+++ server/api/convert-markdown-to-content.post.ts
@@ -1,15 +1,19 @@
-import MarkdownIt from 'markdown-it';
+import MarkdownIt from "markdown-it";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const markdownContent = body.markdown as string;
 
-  const md = new MarkdownIt();
+  const md = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+  });
   const htmlContent = md.render(markdownContent);
 
-  console.log(htmlContent)
+  console.log(htmlContent);
 
   return {
-    html: htmlContent
+    html: htmlContent,
   };
 });
